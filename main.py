d5e1f22a6f2b79dc0602a2e69c95abd5fde52dbd
ebrahim <abrhymjwyd19@gmail.com>
2026-05-18 02:45:15 +0400

    got tiles to apper on screen

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ BACKGROUND_COLOR = (205,192,180)
 FONT_COLOR = (119,110,101)
 
 
-FONT = pygame.font.SysFont("IMPACT", 60 , bold = True)
+FONT = pygame.font.SysFont("comic  sans", 60 , bold = True)
 MOVE_VEL = 20
 
 
@@ -46,6 +46,29 @@ class Tile:
     ]
 
     def __init__ (self, value , row ,col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col* RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+    def get_color(self):
+        color_index = int(math.log2(self.value)) -1
+        color = self.COLORS[color_index]
+        return color
+        
+    
+    def draw(self, window):
+        color = self.get_color()
+        pygame.draw.rect(window , color , (self.x , self.y, RECT_WIDTH, RECT_HEIGHT))
+        
+        text = FONT.render(str(self.value),1 , FONT_COLOR)
+        window.blit(text, (self.x + (RECT_WIDTH /2 - text.get_width()/ 2), self.y + (RECT_HEIGHT /2 - text.get_height() / 2 )) )
+
+    def set_pos(self ):
+        pass
+
+    def move (self, delta):
         pass
 
 
@@ -63,8 +86,13 @@ def drawgrid(window):
     pygame.draw.rect(window, OUTLINE_COLOR, (0,0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
 
 
-def draw(window):
+def draw(window, tiles):
     window.fill(BACKGROUND_COLOR)
+
+
+    for tile in tiles.values():
+     tile.draw(window)
+
     drawgrid(window)
 
     pygame.display.update()
@@ -73,6 +101,8 @@ def draw(window):
 
 def main(window):
     clock = pygame.time.Clock()
+
+    tiles = {"00 ":Tile(4,0,0), "20": Tile(128,2,0)}
 
     run = True
     while run:
@@ -83,7 +113,7 @@ def main(window):
                 run = False
                 break
 
-        draw(window)
+        draw(window, tiles)
     
     
 
